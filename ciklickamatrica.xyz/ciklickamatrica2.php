7279c66dd807bed80052fc1e98a7d11a8d73c409
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2022-01-11 18:52:35 +0100

    unos sintakse za ispis tablice

--- ciklickamatrica.xyz/ciklickamatrica2.php
+++ ciklickamatrica.xyz/ciklickamatrica2.php
@@ -5,72 +5,82 @@
 </head>
 <body>
 
-    <form class="form" action="ciklickmatrica.php" method="get">
+    <form class="form" action="ciklickmatrica2.php" method="get">
         Broj redaka: <input type="number" name="brojRedaka">
         Broj stupaca: <input type="number" name="brojStupaca">
-        <input class="button success" type="submit">
+        <input class="button success" type="submit" value="Izradi tablicu">
     </form>
 
     <?php
 
-    if (isset($_GET['brojStupaca'])){
-        $bs = $_GET['brojStupaca'];
-    }else{
-        $bs = 0;
-    }
-
-    if (isset($_GET['brojStupaca'])){
-        $bs = $_GET['brojStupaca'];
-    }else{
-        $bs = 0;
-    }
-
-    // definiranje veličine tablice/broja polja
-    $bp = $br * $bs;
-
-    // decrement vrijednosti kako bi se sačuvao niz
-
-    $br--;
-    $bs--;
-
-    // postavljanje koordinata matrice
-
-    $minr=0;
-    $mins=0;
-    $maxr=$br;
-    $maxs=$bs;
-
-    // brojač za ispis ćelija
-
-    $p = 1;
-
-    // kreiranje matrice
-
-    $matrica=[];
-
-    while($p<=$bp){
-        for($j=moinr;$j<=$maxr;$j++){
-            $matrica[$maxr][$j]=$p++;
+        if (isset($_GET['brojRedaka'])){
+            $br = $_GET['brojRedaka'];
+        }else{
+            $br = 0;
         }
 
-        if($p>=$bp) break;
-
-        for($i=$maxs;$i>=$minr;$i--){
-            $matrica[$i][$minr]=$p++;
+        if (isset($_GET['brojStupaca'])){
+            $bs = $_GET['brojStupaca'];
+        }else{
+            $bs = 0;
         }
 
-        if($p>=$bp) break;
+        // definiranje veličine tablice/broja polja
+        $bp = $br * $bs;
 
-        for($j=$maxr;$j>=$minr;$j--){
-            $matrica[$minr][$j]=$p--;
+        // decrement vrijednosti kako bi se sačuvao niz
+
+        $br--;
+        $bs--;
+
+        // postavljanje koordinata matrice
+
+        $minr=0;
+        $mins=0;
+        $maxr=$br;
+        $maxs=$bs;
+
+        // brojač za ispis ćelija
+
+        $p = 1;
+
+        // kreiranje matrice
+
+        $matrica=[];
+
+        while($p<=$bp){
+            for($j=moinr;$j<=$maxr;$j++){
+                $matrica[$maxr][$j]=$p++;
+            }
+
+            if($p>=$bp) break;
+
+            for($i=$maxs;$i>=$minr;$i--){
+                $matrica[$i][$minr]=$p++;
+            }
+
+            if($p>=$bp) break;
+
+            for($j=$maxr;$j>=$minr;$j--){
+                $matrica[$minr][$j]=$p--;
+            }
+
+            if($p>=$bp) break;
+
+            for($i=$mins;$i<=$maxs;$i++){
+                $matrica[$i][$maxs]=$p++;
+            }
         }
 
-        if($p>=$bp) break;
-
-        for($i=$mins;$i<=$maxs;$i++){
-            $matrica[$i][$maxs]=$p++;
-        }
-    }
+        echo '<table>';
+            for($i=0;$i<=$br;$i++){
+                echo '<tr>';
+                    for($j=0;$j<=$bs;$j++){
+                        echo '<td>' . $matrica[$i][$j] . '</td>';
+                    }
+                echo '</tr>';
+            }
+        echo '</table>';
         
 
 
