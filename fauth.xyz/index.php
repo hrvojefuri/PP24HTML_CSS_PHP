66e6f73e5a5c8fc9ceb98124dacd8f57a78fc4e6
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2022-01-13 19:18:31 +0100

    unos sintakse za preusmjerenje nakon autorizacije
    unos sintakse za odjavu sa stranice

--- fauth.xyz/index.php
+++ fauth.xyz/index.php
@@ -7,11 +7,24 @@
 
   <body>
 
-    <form action="autoriziraj.php" method="post">
-      <label for="id">ID</label>
-      <input type="text" name="id" />
-      <input type="submit" value="Prijava">
-    </form>
+  <form action="autoriziraj.php">
+  <div class="grid-container">
+    <div class="grid-x grid-padding-x">
+      <div class="medium-12 cell">
+        <label>ID
+          <input type="text" placeholder="ID">
+        </label>
+      </div>      
+    </div>
+  </div>
+  <div class="grid-container">
+    <div class="grid-x grid-padding-x">
+      <div class="medium-12 cell">
+      <button class="submit success button">Prijava</button>
+      </div>      
+    </div>
+  </div>  
+</form>
     
     <?php include_once 'skripte.php'; ?>
   </body>
---
+++ fauth.xyz/privatno.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once 'zaglavlje.php'; ?>
+</head>
+<body>
+    
+    <a href="odjava.php" class="button">Odjavi se</a>
+
+    <?php include_once 'skripte.php'; ?>
+</body>
+</html>
