68046ae700f6c6d4ff571fbce94573ed859f6b6f
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2022-02-06 21:41:21 +0100

    postavljanje metode izbornik

--- mojTerminalApp/Program.php
+++ mojTerminalApp/Program.php
@@ -11,6 +11,40 @@ class Program
         $this->naslov();
     }
 
+    public function izbornik()
+    {
+        echo '1. Pregled proizvoda' . PHP_EOL;
+        echo '2. Unos proizvoda' . PHP_EOL;
+        echo '3. Promjena proizvoda' . PHP_EOL;
+        echo '4. Brisanje proizvoda' . PHP_EOL;
+        echo '5. Izlaz iz programa' . PHP_EOL;
+        $izbor=0;
+        while(true){
+            $izbor = Unos::ucitajInt('Odaberi stavku izbornika: ','Nisi unio cijeli broj');
+            if($izbor<1 || $izbor>5){
+                echo 'Nisi unio moguć izbor' . PHP_EOL;
+                continue;
+            }
+            break;
+        }
+        switch($izbor){
+            case 1:
+                $this->pregledProizvoda();
+                break;
+            case 2:
+                $this->unosProizvoda();
+                break;
+            case 3:
+                $this->promjenaProizvoda();
+                break;
+            case 4:
+                $this->brisanjeProizvoda();
+                break;
+            case 5:
+                echo 'Hvala na korištenju aplikacije, doviđenja!';
+        }
+    }
+
     private function naslov()
 {
     echo '#######################' . PHP_EOL;
